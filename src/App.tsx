fe4a8a6412a6965be64e8dea532c61db47f4847a
sungwon.cha <mygenie3@naver.com>
2019-09-27 09:28:02 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Map from './Map';
+import GithubCorner from 'react-github-corner';
+
+function App() {
+  return (
+    <div>
+      <Map />
+      <GithubCorner href="https://github.com/bluewings/koholint" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Map/Map.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import styles from './Map.module.scss';
+
+function Map(props: any) {
+  return (
+    <div className={styles.root}>
+      <h1>Map</h1>
+    </div>
+  );
+}
+
+export default Map;
