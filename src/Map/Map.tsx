4aee1fa764a953089234bb6d0fb01b568cc0a244
sungwon.cha <mygenie3@naver.com>
2019-09-27 10:21:25 +0900

    Add map tiles

--- src/Map/Map.tsx
+++ src/Map/Map.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
+import useLeaflet from './useLeaflet';
 import styles from './Map.module.scss';
 
 function Map(props: any) {
+  const L = useLeaflet();
+
+  console.log(L);
   return (
     <div className={styles.root}>
-      <h1>Map</h1>
+      {/* <h1>koholint</h1> */}
     </div>
   );
 }
---
+++ src/Map/useLeaflet.ts
@@ -0,0 +1,58 @@
+import { useEffect, useState, useMemo } from 'react';
+import { Map, MapOptions, LatLngExpression } from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+interface InitializeMap {
+  (element: string | HTMLElement, options?: MapOptions): Map;
+}
+
+interface LatLngBounds {
+  new (southWest: LatLngExpression, northEast: LatLngExpression): any;
+}
+
+export type Leaflet = {
+  map: InitializeMap;
+  LatLngBounds: LatLngBounds;
+  Control: any;
+  TileLayer: any;
+};
+
+const LEAFLET_URI = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.5.1/leaflet.js';
+
+const loadScript = (src: string) => {
+  return new Promise((resolve, reject) => {
+    const head = document.head || document.getElementsByTagName('head')[0];
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = src;
+    script.onload = resolve;
+    script.onerror = () => reject(new Error(`failed to load: ${src.split('/').pop()}`));
+    head.appendChild(script);
+  });
+};
+
+function useLeaflet(): Leaflet | undefined {
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      // @ts-ignore
+      if (!window.L) {
+        await loadScript(LEAFLET_URI);
+      }
+      setReady(true);
+    })();
+  }, []);
+
+  let L: any;
+  try {
+    // @ts-ignore
+    L = window.L;
+  } catch (err) {
+    /* ignore */
+  }
+
+  return useMemo(() => (ready && L ? L : undefined), [ready, L]);
+}
+
+export default useLeaflet;
